22296b9f9ee46122f8ba213758aa96cfd89f2482
Meigyoku Thmn <tranhuynhminhngoc1994@gmail.com>
2021-03-30 13:16:38 +0700

    embed Microsoft HTML Object Library Wrapper

--- QuickWebBrowser/ExtendedWebBrowser2/Adblock.cs
+++ QuickWebBrowser/ExtendedWebBrowser2/Adblock.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using MSHTML;
+using mshtml;
 
 namespace ExtendedWebBrowser2
 {
@@ -8,34 +8,24 @@ namespace ExtendedWebBrowser2
     {
         internal static void Filter(HTMLDocument document)
         {
-            foreach (object obj in document.images)
+            foreach (IHTMLImgElement image in document.images)
+                Remove(image as IHTMLDOMNode);
+
+            foreach (IHTMLEmbedElement embed in document.embeds)
+                Remove(embed as IHTMLDOMNode);
+
+            foreach (IHTMLElement2 htmlElement2 in document.getElementsByTagName("OBJECT"))
             {
-                IHTMLImgElement ihtmlimgElement = (IHTMLImgElement)obj;
-                Remove(ihtmlimgElement as IHTMLDOMNode);
-            }
-            foreach (object obj2 in document.embeds)
-            {
-                IHTMLEmbedElement ihtmlembedElement = (IHTMLEmbedElement)obj2;
-                Remove(ihtmlembedElement as IHTMLDOMNode);
-            }
-            foreach (object obj3 in document.getElementsByTagName("OBJECT"))
-            {
-                IHTMLElement2 ihtmlelement = (IHTMLElement2)obj3;
-                foreach (object obj4 in ihtmlelement.getElementsByTagName("PARAM"))
+                foreach (IHTMLElement4 htmlElement4 in htmlElement2.getElementsByTagName("PARAM"))
                 {
-                    IHTMLElement4 ihtmlelement2 = (IHTMLElement4)obj4;
-                    if ("Src".Equals((string)ihtmlelement2.getAttributeNode("NAME").nodeValue, StringComparison.CurrentCultureIgnoreCase)
-                        || "Movie".Equals((string)ihtmlelement2.getAttributeNode("NAME").nodeValue, StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        Remove(ihtmlelement as IHTMLDOMNode);
-                    }
+                    var attr = (string)htmlElement4.getAttributeNode("NAME").nodeValue;
+                    if ("Src".Equals(attr, StringComparison.CurrentCultureIgnoreCase) 
+                        || "Movie".Equals(attr, StringComparison.CurrentCultureIgnoreCase))
+                        Remove(htmlElement2 as IHTMLDOMNode);
                 }
             }
-            foreach (object obj5 in document.getElementsByTagName("IFRAME"))
-            {
-                IHTMLElement4 ihtmlelement3 = (IHTMLElement4)obj5;
-                Remove(ihtmlelement3 as IHTMLDOMNode);
-            }
+            foreach (IHTMLElement4 htmlElement4 in document.getElementsByTagName("IFRAME"))
+                Remove(htmlElement4 as IHTMLDOMNode);
         }
 
         private static void Remove(IHTMLDOMNode node)
--- QuickWebBrowser/ExtendedWebBrowser2/BrowserControl.cs
+++ QuickWebBrowser/ExtendedWebBrowser2/BrowserControl.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using mshtml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
-using MSHTML;
 
 namespace ExtendedWebBrowser2
 {
